8c24aadf0c2433a9acb9415030a579d6e75d95e6
Phygson <124876018+Phygson@users.noreply.github.com>
2024-04-07 21:12:59 +0300

    filer notes on the fretboard

--- src/chshape.cc
+++ src/chshape.cc
@@ -24,12 +24,32 @@ std::string impl(std::vector<Note>& notes) {
         for (int j = 0; j < grf[i].size(); j++) {
             if (std::find(qq.begin(), qq.end(), grf[i][j]) != qq.end()) pp[i].push_back(std::make_pair(grf[i][j], j));
         }
-    } 
+    }
 
-    return std::to_string(pp[3][0].second);
+    auto res = std::string();
+
+    for (int tt = 0; tt < 9; tt++) {
+        std::vector<std::vector<std::pair<int, int>>> filt(6);
+        for (int i = 0; i < pp.size(); i++) {
+            for (int j = 0; j < pp[i].size(); j++) {
+                if (pp[i][j].second == 0 || (pp[i][j].second >= tt && pp[i][j].second < tt+4)) filt[i].push_back(pp[i][j]);
+            }
+        }
+        for (auto& a : filt) {
+            for (auto& b : a) {
+                res += Note(b.first).get() + "("; 
+                res += std::to_string(b.second)+")";
+                res += " ";
+            }
+            res += "\t";
+        }
+        res += "\n";
+    }
+
+    return res;
 }
 
 std::string getChordShape(const std::string &name)
 {
-    return impl(std::vector<Note>({Note("C"), Note("E"), Note("G"), Note("B")}));
+    return impl(std::vector<Note>({Note("C"), Note("E"), Note("G")}));
 }
